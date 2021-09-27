6c974ddd1501911bcd9c8e0d27335996dd294859
Sean Sutton <sutton_mjbc@icloud.com>
2021-09-27 08:26:04 -0400

    setting up form inputs & validations #time 1h

--- screens/Home.js
+++ screens/Home.js
@@ -13,46 +13,68 @@ let validationSchema = yup.object().shape({
 });
 
 const Home = ( {props, navigation} ) => {
+
+  const handeSubmitForm = (values) => {
+    // handle post submit form with values
+    console.log('SUBMIT', values)
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Enter your Farmer Info!</Text>
       <Formik
         initialValues={{name: '', email: '', farmName: ''}}
         validateOnMount={true}
-        onSubmit={values => console.log(values)}
+        onSubmit={values => {
+          console.log(values)
+        }}
+        validationSchema={validationSchema}
       >
       {
         ({ handleChange, handleBlur, handleSubmit, values, touched, errors, isValid }) => (
-          <ScrollView
-            style={{flex: 1, backgroundColor: '#fefefe'}}
-            showsVerticalScrollIndicator={false}
-          >
+          <View style={styles.container}>
             <View>
               <TextInput
+                style={styles.input}
                 onChangeText={handleChange('name')}
                 placeholder='Name'
                 onBlur={handleBlur('name')}
                 value={values.name}
               />
+              {(errors.name && touched.name) && (
+                <Text style={styles.errors}>{errors.name}</Text>
+              )}
             </View>
             <View>
               <TextInput
+                style={styles.input}
                 onChangeText={handleChange('email')}
                 placeholder='Enter Email'
                 onBlur={handleBlur('email')}
                 value={values.email}
               />
+              {(errors.email && touched.email) && (
+                <Text style={styles.errors}>{errors.email}</Text>
+              )}
             </View>
             <View>
               <TextInput
+                style={styles.input}
                 onChangeText={handleChange('farmName')}
                 placeholder='Farm Name'
                 onBlur={handleBlur('farmName')}
                 value={values.farmName}
               />
+              {(errors.farmName && touched.farmName) && (
+                <Text style={styles.errors}>{errors.farmName}</Text>
+              )}
             </View>
-            <Button onPress={() => navigation.navigate('FarmerScreen')} title='Submit' />
-          </ScrollView>
+            <Button 
+              style={[styles.button, { color: isValid ? '#4632A1' : '#CACFD2' }]}
+              disabled={!isValid}
+              rounded 
+              onPress={handleSubmit} title='Submit' />
+            </View>
         )
       }
       </Formik>
@@ -61,3 +83,27 @@ const Home = ( {props, navigation} ) => {
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  button: {
+    shadowColor: '#00acee',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 6
+  },
+  errors: {
+    fontSize: 14,
+    color: 'red',
+    fontWeight: 'bold',
+    marginTop: 5
+  }
+})
